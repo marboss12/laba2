7a50e17886cbacbdc0b2a49857364bac1d6136f3
marboss12 <marcelfederov@gmail.com>
2025-05-01 11:47:54 +0300

    hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,8 @@
     int main() {
         std::string name;
         std::cout << "Введите ваше имя: ";
-        std::cin >> name;
+        std::cin >> name; //ввод имени пользователя
         std::cout << "Hello, world from @" << name << std::endl;
+      // Используем std::cout и std::endl для избежания конфликтов имен
         return 0;
     }
