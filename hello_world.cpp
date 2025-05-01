30c2f71d426b715d531b47e5bb67d6589194b0ca
Marsel1010 <zurabichmarcel@gmail.com>
2025-05-01 11:38:48 +0300

    Fix: Remove using namespace std and fix code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,9 @@
-    #include <iostream>
+  #include <iostream>
     #include <string>
-    using namespace std;
-
     int main() {
-        string name;
-        cout << "Введите ваше имя: ";
-        cin >> name;
-        cout << "Hello, world from @" << name << endl;
+        std::string name;
+        std::cout << "Введите ваше имя: ";
+        std::cin >> name;
+        std::cout << "Hello, world from @" << name << std::endl;
         return 0;
     }
-    
-
