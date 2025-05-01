dc559508272da6f44093574103f72b9a091193c4
Marsel1010 <zurabichmarcel@gmail.com>
2025-05-01 10:44:02 +0300

    Prompt for user name and personalize greeting

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
-}
+    #include <iostream>
+    #include <string>
+    using namespace std;
+
+    int main() {
+        string name;
+        cout << "Введите ваше имя: ";
+        cin >> name;
+        cout << "Hello, world from @" << name << endl;
+        return 0;
+    }
+    
+
