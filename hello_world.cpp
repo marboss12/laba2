2b17fe639fd59fac4c77d219f3041bc6e01166fa
marboss12 <marcelfederov@gmail.com>
2025-05-01 13:56:19 +0300

    Update1 hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
-//программа ввода и вывода имени пользователя
+//программа ввода и вывода имени пользователя!!!
 #include <iostream>
     #include <string> 
     int main() {
         std::string name;
         std::cout << "Введите ваше имя: ";
-        std::cin >> name; //ввод имени пользователя
+        std::cin >> name; //ввод имени пользователя!!!!
         std::cout << "Hello, world from @" << name << std::endl;
-      // Используем std::cout и std::endl для избежания конфликтов имен
+      // Используем std::cout и std::endl для избежания конфликтов имен!&!!&
         return 0;
     }
